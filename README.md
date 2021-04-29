39158b66f281143e43aab917fa0af13fa63c04d0
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2021-04-29 16:15:21 +0200

    Bump CrysFML to 44788

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # libsWin32
 
-CFML: <https://code.ill.fr/scientific-software/crysfml/-/jobs/44539>
+CFML: <https://code.ill.fr/scientific-software/crysfml/-/jobs/44788>
--- libsWin32/CFML_api/API_Reflections_Utilities.py
+++ libsWin32/CFML_api/API_Reflections_Utilities.py
@@ -27,6 +27,15 @@ class ReflectionList(CFML_api.FortranBindedClass):
         
     def __del__(self):
         CFML_api.crysfml_api.reflections_utilities_del_reflection_list(self.get_fortran_address())
+
+    def __getitem__(self,key):
+        dict = CFML_api.crysfml_api.reflections_utilities_get_item(self.get_fortran_address(),key)
+        ref = CFML_api.API_Reflections_Utilities.Reflection.from_fortran_address(dict["Ref"])
+        return ref
+
+    @property
+    def nrefs(self):
+        return CFML_api.crysfml_api.reflections_utilities_get_nref(self.get_fortran_address())["nref"]
     
     def compute_structure_factors(self, space_group, atom_list, job):
                             
@@ -34,3 +43,91 @@ class ReflectionList(CFML_api.FortranBindedClass):
             atom_list.get_fortran_address(), space_group.get_fortran_address(),
             self.get_fortran_address(), job.get_fortran_address())
         
+
+class Reflection(CFML_api.FortranBindedClass):
+    """ Class for a given Reflection in CFML
+
+    ... 
+    Attributes
+    ----------
+
+    Methods
+    -------
+    """
+    
+    def __init__(self):
+        CFML_api.FortranBindedClass.__init__(self)
+
+    def __del__(self):
+        CFML_api.crysfml_api.reflections_utilities_del_reflection(self.get_fortran_address())
+    
+    @property
+    def hkl(self):
+        """
+        hkl indices
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_H(self.get_fortran_address())["H"]
+    
+    @property
+    def multiplicity(self):
+        """
+        Multiplicity
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_Mult(self.get_fortran_address())["Mult"]
+    
+    @property
+    def fobs(self):
+        """
+        Observed structure factor
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_Fo(self.get_fortran_address())["Fo"]
+    
+    @property
+    def fcalc(self):
+        """
+        Calculated structure factor
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_Fc(self.get_fortran_address())["Fc"]
+    
+    @property
+    def sigma_fobs(self):
+        """
+        Sigma for the Observed structure factor
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_SFo(self.get_fortran_address())["SFo"]
+    
+    @property
+    def stl(self):
+        """
+        sin(theta)/lambda
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_S(self.get_fortran_address())["S"]
+    
+    @property
+    def weight(self):
+        """
+        weight
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_W(self.get_fortran_address())["W"]
+    
+    @property
+    def phase(self):
+        """
+        Phase in degrees
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_Phase(self.get_fortran_address())["Phase"]
+    
+    @property
+    def a(self):
+        """
+        real part of the Structure Factor
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_A(self.get_fortran_address())["A"]
+    
+    @property
+    def b(self):
+        """
+        imaginary part of the structure factor
+        """
+        return CFML_api.crysfml_api.reflections_utilities_get_B(self.get_fortran_address())["B"]
+    
